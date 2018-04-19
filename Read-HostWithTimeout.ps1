ec819064f760ca44309780afc5d7b99af0118f26
fereste <fernando.piubel@gmail.com>
2018-04-19 00:10:10 -0300

    Updated help comment

--- Read-HostWithTimeout.ps1
+++ Read-HostWithTimeout.ps1
@@ -4,25 +4,22 @@ function Read-HostWithTimeout {
     Text input with timeout.
 
     .DESCRIPTION
-    An unholy union of $host.UI.RawUI.ReadKey() and Read-Host.  It is usable, but that's good enough for my current needs.
-    Here are the limitations I've hit so far:
-    - Keyboard buffer doesn't.  If you type too fast, the second (or more) character(s) will be lost.
-    - Keyboard buffer is not contiguious.  The first character is captured by ReadKey(), and the rest by Read-Host.  This means up-arrow will only show the characters captured by Read-Host.
-    - This also means you can't back up over the first character.
-    Still, all I need is a Y/N with a timeout, and that works.
+    Text input with timeout. If a key is not pressed before the configured timeout, input will end.
     
     .PARAMETER Prompt
     Prompt for input.  Will have a ': ' appended, but no linefeed.
     
     .PARAMETER Timeout
-    Seconds to wait for input.  If no input received, will return $null.  If the -NoNewLine parameter is not specified, once the user starts typing, the timeout is suspended until the user presses [Enter].
+    Seconds to wait for input.
     
     .NOTES
     Based on https://blogs.msdn.microsoft.com/timid/2014/01/29/read-host-with-a-timeout-kind-of/
     #>
 
     Param (
+        [Parameter(Mandatory = $false)]
         [string]$Prompt = '',
+        [Parameter(Mandatory = $true)]
         [ValidateRange(1, 60)]
         [int]$Timeout = 10
     );
